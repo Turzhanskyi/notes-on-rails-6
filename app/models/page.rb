f0a9e909b4e2f90bac61adfe99f0b75f9206b85a
Vitalii <turzhansky81@gmail.com>
2020-08-22 08:00:59 +0300

    Update render partials as elements

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,8 @@ class Page < ApplicationRecord
   has_many :paragraphs, dependent: :destroy
   has_many :image_elements, dependent: :destroy
   has_many :checklists, dependent: :destroy
+
+  def elements
+    paragraphs + image_elements + checklists
+  end
 end
